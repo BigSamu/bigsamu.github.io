dfe4c31fb3059a2a67d515ca92dae0724818ea0a
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2023-10-30 18:13:45 +0000

    changing github link AMG Portfolio

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -14,12 +14,12 @@ export default [
     title: 'AMG Portfolio',
     imgUrl: '/assets/projects/amg_portfolio.png',
     stack: ['HTML', 'JavaScript', 'ReactJS', 'ViteJS', 'TailwindCSS'],
-    repo: 'https://github.com/BigSamu/Teaching_ML_Kaggle_Style',
+    repo: 'https://github.com/BigSamu/AMG_Portfolio',
     demo: 'https://anamariagutierrez.art',
     details:
       'A web portfolio for my mother, who is an artist! Simple and minimalist design used to showcase her work. Project powered by ReactJS, ViteJS and TailwindCSS.',
     type: 'Solo Project (2023) - In Progress',
-    
+
   },
   {
     title: 'ChatGPT Image Creator',
@@ -30,7 +30,7 @@ export default [
     details:
       "A simple project that uses OpenAI's API to generate images based on textual prompts. My first attempt at using OpenAI's API and my first project using ViteJS.",
     type: 'Solo Project (2023)',
-   
+
   },
   {
     title: 'SeisEnt',
@@ -41,7 +41,7 @@ export default [
     details:
       'Proof-of-Concept (Full-Stack application) presenting a solution for seismic data entitlement problem in the Oil and Gas industry. Use of JWT and Macaroons technologies and Business Rules Engines to approach solution for streamline compliance of data entitlement.',
     type: 'MSc CS degree - Thesis Dissertation (2022)',
-    
+
   },
   {
     title: 'TeachingML - Kaggle Style',
@@ -52,7 +52,7 @@ export default [
     details:
       'Web Application solution for clarifying ML overfitting concept to students from Imperial College Business School. Project as part of a group assignment from MSc CS degree, being in charge of the development of the frontend side of the application.',
     type: 'MSc CS degree - Group Project (2021)',
-    
+
   },
   {
     title: '567 Bakery',
@@ -63,6 +63,6 @@ export default [
     details:
       'My first end-to-end project! A simple vanilla website for a local bakery in my hometown. Implement all learnings from my Bootcamp at Coding Dojo, including external libraries such as Bootstrap, Saas, JQuery and Swipper.',
     type: 'Online Bootcamp - Final Project (2020)',
-    
+
   },
 ];
--- src/data/userDetails.js
+++ src/data/userDetails.js
@@ -2,6 +2,4 @@ export default {
   fullname: 'Samuel Valdes Gutierrez',
   nickname: 'BigSamu',
   cv_url: 'https://1drv.ms/b/s!Asber9wcpjf8kZ1sFoVj5UKjMjpsDg?e=vKI54K',
-
-
 };
